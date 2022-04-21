2abe09b78f473488258bc6101da7cb7453965143
rodriguezal3x <alexjosuerm1995@gmail.com>
2022-04-21 01:36:17 -0600

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <header>
+        <Navbar/>
+    </header>
+  )
+}
+
+export default Home
